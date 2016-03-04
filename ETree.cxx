7f668f9f78e3f0041f0b82248be61dd3bce962c6
eric_mann <eric@deep.is>
2016-03-04 11:37:00 -0500

    initial add of ETree files

---
+++ ETree.cxx
@@ -0,0 +1,7 @@
+#include "ETree.h"
+using namespace utils;
+
+int main(int argc, char** argv) {
+	ETree* t = new ETree();
+	delete t;
+}
---
+++ ETree.h
@@ -0,0 +1,12 @@
+#ifndef _E_TREE_H_ 
+#define _E_TREE_H_ 
+
+namespace utils {
+
+	class ETree {
+
+	}; // class ETree
+
+} // namespace
+
+#endif // define 
